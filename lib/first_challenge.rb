d75a7017cb26840e694d3ca0a0fb6833d865ec6f
Will Carter <will.carter@fergusllc.com>
2019-12-29 23:26:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -9,13 +9,27 @@ def first_challenge
          :name => "Lady Montague",
          :age => "54"
        },
-       :hero => {},
+       :hero => {
+         :name => "Romeo",
+         :age => "15",
+         :status => "alive".
+       },
        :hero_friends => []
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
+       :patriarch => {
+         :name => "Lord Capulet",
+         :age => "50"
+       },
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age =>"51"
+       },
+       :heroine => {
+         :name => "Juliet", 
+         :age => "15",
+         :status => "alive"
+       },
        :heroine_friends => []
      }
    }
